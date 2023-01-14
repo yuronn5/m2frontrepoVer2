e6c22cb91694c1896b5d277db9223b74a0eb22cb
yurii_bilous007 <yurii.b@elogic.com.ua>
2023-01-14 14:28:33 -0800

    Ui course 15 uiregistry set get remove examples

--- app/code/Macademy/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ app/code/Macademy/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -21,3 +21,6 @@ define([
 
 
 //require('uiRegistry').get("free-shipping-banner")     Ui registry tutorial
+// require('uiRegistry').get("free-shipping-banner").set('hello', 'world') - set new param hello and his value world
+// require('uiRegistry').get("free-shipping-banner").set('welcome', {to: {my: 'world'}})  set the same but for object
+// require('uiRegistry').get("free-shipping-banner").remove('welcome.to.my')  remove
