dd342a6607b1c70f3f3ac032c1de358fdf8575af
yurii_bilous007 <yurii.b@elogic.com.ua>
2023-01-14 17:06:07 -0800

    Ui course 25 added overrided template file for mixin

--- app/code/Macademy/CheckoutMessages/view/frontend/web/js/view/summary/cart-items-mixin.js
+++ app/code/Macademy/CheckoutMessages/view/frontend/web/js/view/summary/cart-items-mixin.js
@@ -1,13 +1,10 @@
- define([], function() {
+  define([], function() {
     'use strict';
 
     return function (Component) {
         return Component.extend({
             defaults: {
-                template: 'Macademy_CheckoutMessages/summary/cart-items',
-                exports: {
-                    'totals.subtotal': 'checkout.sidebar.guarantee:subtotal'
-                }
+                template: 'Macademy_CheckoutMessages/summary/cart-items'
             },
             isItemsBlockExpanded: function() {
                 // If you wish to execute parent method, be sure to call
